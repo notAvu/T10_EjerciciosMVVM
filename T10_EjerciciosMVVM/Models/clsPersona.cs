8b115b6916adeb8a2583b28e679d1f89dc6ce3ea
Álvaro Fernández <avunotfound@gmail.com>
2021-10-25 14:36:20 +0200

    Commit inicial

---
+++ T10_EjerciciosMVVM/Models/clsPersona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T10_EjerciciosMVVM.Models
+{
+    public class clsPersona
+    {
+        string nombre;
+        string apellido;
+
+        clsPersona() 
+        {
+            nombre = "Alvaro";
+            apellido = "Fernandez Villar";
+        }
+
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+}
---
+++ T10_EjerciciosMVVM/ViewModels/Ejercicio2VM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T10_EjerciciosMVVM.Models;
+
+namespace T10_EjerciciosMVVM.ViewModels
+{
+    public class Ejercicio2VM
+    {
+        clsPersona oPersona;
+
+        public clsPersona OPersona { get => oPersona; set => oPersona = value; }
+    }
+}
