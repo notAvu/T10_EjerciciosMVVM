e71093be932b464fdd3a031898ef48476e550840
Álvaro Fernández <avunotfound@gmail.com>
2021-10-27 13:20:32 +0200

    Troleito

--- T10_EjerciciosMVVM/Models/clsPersona.cs
+++ T10_EjerciciosMVVM/Models/clsPersona.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace T10_EjerciciosMVVM.Models
 {
-    public class clsPersona
+    public class clsPersona :INotifyPropertyChanged
     {
         #region propiedades privadas
         private string nombre;
@@ -22,17 +24,48 @@ namespace T10_EjerciciosMVVM.Models
         #region propiedades publicas
         public string Apellido
         { 
-            get { return apellido; } 
-            set { apellido = value;} 
+            get {
+                
+                return apellido; 
+            } 
+            set {
+                if (value.Contains('n'))
+                { 
+                    nombre = "";
+                    NotifyPropertyChanged("Nombre");
+                }
+                apellido = value;
+            }
         }
         public string Nombre 
         { 
-            get{ return nombre; } 
-            set{ nombre = value; } 
+            get
+            {
+                
+                return nombre; 
+            }
+            set
+            {
+                if (value.Contains('n'))
+                { 
+                    apellido = "";
+                    NotifyPropertyChanged("Apellido");
+                }
+                nombre = value;
+
+            }
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
         #region metodos
-
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
         #endregion
     }
 }
